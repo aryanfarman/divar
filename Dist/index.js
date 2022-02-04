1a62a5e9789ffc58b5529b78ed87d6502e93ac14
aryan <aryan22farman@gmail.com>
2022-02-04 20:27:30 +0330

    dist ignored

--- Dist/index.js
+++ Dist/index.js
@@ -14,48 +14,14 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.getNames = exports.getBrands = exports.consoleEverySpecificTime = exports.getEverySpecificTime = exports.getCars = void 0;
 const got_1 = __importDefault(require("got"));
-const express_1 = __importDefault(require("express"));
-const helmet_1 = __importDefault(require("helmet"));
 const debug_1 = __importDefault(require("debug"));
 const brands_1 = require("./models/brands");
-const carRoutes_1 = require("./routes/carRoutes");
-const brandRoutes_1 = require("./routes/brandRoutes");
 const cars_1 = require("./models/cars");
 const persian_tools_1 = require("@persian-tools/persian-tools");
-const port = process.env.PORT || 3000;
-let str = "";
-/*
-import cheerio, {CheerioAPI, Node} from 'cheerio';
-import request from 'request-promise';
-
- const getCat=async (url:string) => {
-        await request('https://divar.ir/s/mashhad/car', (error: any, response: { statusCode: number; }, html: string | Node | Node[] | Buffer )=>{
-            if(!error && response.statusCode===200){
-                const $ = cheerio.load(html);
-                const data = $(".kt-internal-link-list__item-content");
-                const datas = Array.from($(data));
-                const hrefs :Array<string>=[] ;
-                datas.forEach((item,i)=>{
-                    // @ts-ignore
-                    hrefs.push(data[`${i}`].attribs.href)
-                })
-                hrefs.forEach((item,i)=> {
-                    brands.push({
-                        id: i+1,
-                        name: item.substr(15)
-                    })
-                   return brands
-                })
-            }
-    })
-}*/
+const globTime = 3000;
 const errorDebug = (0, debug_1.default)("app:error");
-const app = (0, express_1.default)();
-app.use(express_1.default.json());
-app.use(helmet_1.default.hidePoweredBy());
 function getCars(carBrand = brands_1.brands[Math.floor(Math.random() * (brands_1.brands.length))].name) {
     return __awaiter(this, void 0, void 0, function* () {
-        console.log('hi');
         try {
             const response = yield (0, got_1.default)(`http://api.divar.ir/v8/web-search/mashhad/car/${carBrand}`);
             const json = JSON.parse(response.body);
@@ -93,18 +59,14 @@ function getCars(carBrand = brands_1.brands[Math.floor(Math.random() * (brands_1
     });
 }
 exports.getCars = getCars;
-// car Routes
-app.use("/api/car", carRoutes_1.carRouter);
-// brand Routes
-app.use("/api/brand", brandRoutes_1.brandRouter);
-const getEverySpecificTime = function (time = carRoutes_1.globTime, carBrand = brands_1.brands[Math.floor(Math.random() * (brands_1.brands.length))].name) {
+const getEverySpecificTime = function (time = globTime, carBrand = brands_1.brands[Math.floor(Math.random() * (brands_1.brands.length))].name) {
     setInterval(() => {
         getCars(carBrand).then();
     }, time);
     return cars_1.cars;
 };
 exports.getEverySpecificTime = getEverySpecificTime;
-const consoleEverySpecificTime = function (time = carRoutes_1.globTime, carBrand = brands_1.brands[Math.floor(Math.random() * (brands_1.brands.length))].name) {
+const consoleEverySpecificTime = function (time = globTime, carBrand = brands_1.brands[Math.floor(Math.random() * (brands_1.brands.length))].name) {
     const logger = (0, debug_1.default)("app:logger");
     setInterval(() => {
         getCars(carBrand).then(() => {
@@ -113,10 +75,6 @@ const consoleEverySpecificTime = function (time = carRoutes_1.globTime, carBrand
     }, time);
 };
 exports.consoleEverySpecificTime = consoleEverySpecificTime;
-const portListener = (0, debug_1.default)("app:listen");
-app.listen(port, () => {
-    portListener("listening to port : " + port);
-});
 const getBrands = function () {
     return brands_1.brands;
 };
